f19607d752abae4a8a23c35ca823c64ccc95a9e3
ThatMasterCoder <youareanoob09@gmail.com>
2025-11-24 15:17:19 -0500

    create frontend

---
+++ frontend/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function main() {
+    console.log("Hello, World!");
+    return "true";
+}
+main();
+console.log("this is typescript code");
---
+++ frontend/ts/main.ts
@@ -0,0 +1,9 @@
+function main(): string {
+    console.log("Hello, World!");
+    return "true";
+}
+
+
+main();
+
+console.log("this is typescript code");
